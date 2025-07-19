3f5834baad7efe75715a4a57ed4e6c0c9b370660
stepienov <stepienovps@gmail.com>
2025-07-19 08:22:11 +0100

    Develop app

--- src/main/java/com/tenedevia/api/mapper/BeachMapper.java
+++ src/main/java/com/tenedevia/api/mapper/BeachMapper.java
@@ -2,14 +2,11 @@ package com.tenedevia.api.mapper;
 
 import com.tenedevia.api.dto.*;
 import com.tenedevia.api.model.*;
-import com.tenedevia.api.model.common.Address;
 import com.tenedevia.api.model.common.GeoPoint;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface BeachMapper {
-  BeachDto toDto(BeachEntity entity);
-  GeoPointDto toDto(GeoPoint geo);
-  AddressDto toDto(Address address);
-  BeachEntity toEntity(BeachDto dto);
+  BeachDto toDto(PoiEntity entity);
+  GeoPointDto toDto(GeoPoint geoPoint);
 }
--- src/main/java/com/tenedevia/api/model/PoiEntity.java
+++ src/main/java/com/tenedevia/api/model/PoiEntity.java
@@ -6,24 +6,29 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "beaches")
-@Data
+@Table(name = "points_of_interest")
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class BeachEntity {
+public class PoiEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(name = "name_of_point")
   private String name;
+
+  @Column(name = "description_of_interest")
   private String description;
 
   @Embedded
+  @AttributeOverrides({
+      @AttributeOverride(name = "latitude", column = @Column(name = "latitude")),
+      @AttributeOverride(name = "longitude", column = @Column(name = "longitude"))
+  })
   private GeoPoint coordinates;
 
-  @Embedded
-  private Address address;
 }
 
