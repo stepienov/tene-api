b491e535cc395b4be571de43311b0e2a5c8d7dab
stepienov <stepienovps@gmail.com>
2025-07-24 22:37:51 +0100

    Trigger deploy

--- src/main/java/com/tenedevia/api/mapper/PoiMapper.java
+++ src/main/java/com/tenedevia/api/mapper/PoiMapper.java
@@ -1,12 +1,17 @@
 package com.tenedevia.api.mapper;
 
 import com.tenedevia.api.dto.*;
-import com.tenedevia.api.model.PoiEntity;
+import com.tenedevia.api.model.*;
 import com.tenedevia.api.model.common.GeoPoint;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PoiMapper {
+
+  @Mapping(target = "municipality", expression = "java(entity.getMunicipality() != null ? entity.getMunicipality().getMuniciDesc() : null)")
+  @Mapping(target = "region", expression = "java(entity.getMunicipality() != null && entity.getMunicipality().getRegion() != null ? entity.getMunicipality().getRegion().getRegionDirectionDesc() : null)")
   PoiDto toDto(PoiEntity entity);
+
   GeoPointDto toDto(GeoPoint geoPoint);
 }
